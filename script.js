b0f264baf97c2a9b09948b4cd202d494fc4f40a0
Rohit Kumar <choudharyrohit313131@gmail.com>
2022-12-11 17:11:48 +0530

    file uploaded

---
+++ script.js
@@ -0,0 +1,100 @@
+const box = document.querySelectorAll('.box');
+
+// const drum = document.querySelector('drum-container');
+
+box[0].addEventListener('mouseover',()=>{
+    const audio =  new Audio('./sounds/button-1.mp3');
+    audio.play();
+});
+box[0].addEventListener('click',()=>{
+    const audio =  new Audio('./sounds/button-1.mp3');
+    audio.play();
+});
+box[1].addEventListener('mouseover',()=>{
+    const audio =  new Audio('./sounds/button-2.mp3');
+    audio.play();
+});
+box[2].addEventListener('mouseover',()=>{
+    const audio =  new Audio('./sounds/button-3.mp3');
+    audio.play();
+});
+
+box[3].addEventListener('mouseover',()=>{
+    const audio =  new Audio('./sounds/button-4.mp3');
+    audio.play();
+});
+
+box[4].addEventListener('mouseover',()=>{
+    const audio =  new Audio('./sounds/button-5.mp3');
+    audio.play();
+});
+
+box[5].addEventListener('mouseover',()=>{
+    const audio =  new Audio('./sounds/button-6.mp3');
+    audio.play();
+});
+
+box[6].addEventListener('mouseover',()=>{
+    const audio =  new Audio('./sounds/button-7.mp3');
+    audio.play();
+});
+
+box[7].addEventListener('mouseover',()=>{
+    const audio =  new Audio('./sounds/button-8.mp3');
+    audio.play();
+});
+
+box[8].addEventListener('mouseover',()=>{
+    const audio =  new Audio('./sounds/button-9.mp3');
+    audio.play();
+});
+box[1].addEventListener('click',()=>{
+    const audio =  new Audio('./sounds/button-2.mp3');
+    audio.play();
+});
+box[2].addEventListener('click',()=>{
+    const audio =  new Audio('./sounds/button-3.mp3');
+    audio.play();
+});
+
+box[3].addEventListener('click',()=>{
+    const audio =  new Audio('./sounds/button-4.mp3');
+    audio.play();
+});
+
+box[4].addEventListener('click',()=>{
+    const audio =  new Audio('./sounds/button-5.mp3');
+    audio.play();
+});
+
+box[5].addEventListener('click',()=>{
+    const audio =  new Audio('./sounds/button-6.mp3');
+    audio.play();
+});
+
+box[6].addEventListener('click',()=>{
+    const audio =  new Audio('./sounds/button-7.mp3');
+    audio.play();
+});
+
+box[7].addEventListener('click',()=>{
+    const audio =  new Audio('./sounds/button-8.mp3');
+    audio.play();
+});
+
+box[8].addEventListener('click',()=>{
+    const audio =  new Audio('./sounds/button-9.mp3');
+    audio.play();
+});
+
+
+// drum.addEventListener('click',()=>{
+
+//     for (let i = 0; i < box.length; i++) {
+//         box[i].addEventListener('click',()=>{
+//             const audio =  new Audio('https://www.soundjay.com/buttons/sounds/button-1.mp3');
+//             audio.play();
+//         });
+        
+//     }
+// });
